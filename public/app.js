fcf349f71ccd21a01eb3041efc7243dfd760c936
nalalalan <pham.alann@gmail.com>
2026-05-18 19:51:51 -0400

    Keep League champion cards first

--- public/app.js
+++ public/app.js
@@ -730,7 +730,7 @@ const fallbackAudioUrls = {};
 let burstTimer = 0;
 let fxTimer = 0;
 const stingerVersion = "20260518-cinematic53";
-const visualFxVersion = "20260518-world-vfx23";
+const visualFxVersion = "20260518-world-vfx24";
 let vfx3dModulePromise;
 const vfx3dWarmPromises = {};
 let vfx2dWarmPromise;
@@ -4924,7 +4924,7 @@ function spawnSelectionFx(button, profileId = "default") {
   const canvas = document.createElement("canvas");
   fx.className = "selection-fx ultimate-scene";
   fx.dataset.championFx = profile.id;
-  fx.dataset.vfxTier = "panel-atmosphere-vfx23";
+  fx.dataset.vfxTier = "panel-atmosphere-vfx24";
   fx.setAttribute("aria-hidden", "true");
   shaderCanvas.className = "fx-shader-canvas";
   threeCanvas.className = "fx-three-canvas";
