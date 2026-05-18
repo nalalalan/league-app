cb17dfa48b2f8fadc810afb094b43ebfbbf1a545
nalalalan <pham.alann@gmail.com>
2026-05-18 01:11:58 -0400

    Retune champion stingers to requested snippets

--- public/app.js
+++ public/app.js
@@ -636,7 +636,7 @@ let activeStinger;
 const fallbackAudioUrls = {};
 let burstTimer = 0;
 let fxTimer = 0;
-const stingerVersion = "20260518-phrase23";
+const stingerVersion = "20260518-phrase24";
 const stingerUrls = Object.fromEntries(champions.map((champion) => [
   champion.id,
   `/audio/${champion.id}.mp3?v=${stingerVersion}`
